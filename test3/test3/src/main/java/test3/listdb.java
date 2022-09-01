1ac8b1e91cbc2af8a58af17ff68885912d34c087
eejang <105977239+eejsh@users.noreply.github.com>
2022-09-01 11:12:52 +0900

    Add files via upload

---
+++ test3/test3/src/main/java/test3/listdb.java
@@ -0,0 +1,58 @@
+package test3;
+
+import java.sql.*;
+
+//db접속
+public class listdb {
+
+	public String dburl = "jdbc:mysql://umj7-009.cafe24.com/eejsh08";
+	public String dbuser = "eejsh08";
+	public String dbpw = "hoho12!@";
+	public Connection con = null;
+	public String dbDriver = "com.mysql.jdbc.Driver";
+	
+	public listdb() {
+
+//		System.out.println(dbservice.mid);
+		
+	}
+
+
+	public String insert(dbservice dbservice) throws SQLException, ClassNotFoundException{
+		String result ="error";
+		
+		String mid = dbservice.mid;
+		String mpw = dbservice.mpw;
+		String usernm = dbservice.usernm;
+		String tel = dbservice.tel;
+		String age = dbservice.age;
+		
+		String sql = "insert into test3(midx,mid,mpw,mnm,mtel,mage)"
+				+ " values('0',?,?,?,?,?)";
+		
+		
+		Class.forName(dbDriver);
+		con = DriverManager.getConnection(dburl,dbuser,dbpw);
+		PreparedStatement pr = con.prepareStatement(sql);
+		pr.setString(1, mid);
+		pr.setString(2, mpw);
+		pr.setString(3, usernm);
+		pr.setString(4, tel);
+		pr.setString(5, age);
+		
+		int n=0;
+        n = pr.executeUpdate();
+        
+        if(n>0) {
+        	result = "ok";
+        }
+        pr.close();
+        con.close();
+        return result;
+		
+	}
+
+
+	
+
+}
---
+++ test3/test3/src/main/webapp/result.jsp
@@ -0,0 +1,16 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+    
+   
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Insert title here</title>
+</head>
+<body>
+외부서버에서 값을 받음 : <%= request.getParameter("result") %>
+
+내부서버에서 값을 받음 : ${call}
+</body>
+</html>
